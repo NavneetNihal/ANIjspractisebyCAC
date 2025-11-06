a8fe3014c77466cfd544f000719d81bf6cc5fd2e
NavneetNihal <lakranihal0070@gmail.com>
2025-11-06 13:28:57 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+// let score = "Nihal"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = "Nihal"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
