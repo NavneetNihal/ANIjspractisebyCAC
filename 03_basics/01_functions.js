65f1c24019cbb0a332aa34e079398b6c9e40b239
NavneetNihal <lakranihal0070@gmail.com>
2025-11-11 18:12:02 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,5 +30,34 @@ function loginUserMessage(username) {
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("Nihal"));
+// console.log(loginUserMessage());
 
-console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Nihal",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+// handleObject(user)
+handleObject({
+    username: "Sunshine",
+    price: "Priceless"
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 500, 600]));
