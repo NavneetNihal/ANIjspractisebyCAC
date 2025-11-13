18a0922d76270d6ee5ef89831d406ee0179965fb
NavneetNihal <lakranihal0070@gmail.com>
2025-11-13 15:04:13 +0000

    one switches truthy

--- 04_control-flow/one.js
+++ 04_control-flow/one.js
@@ -1,8 +1,45 @@
 // if
 
-const isUserLoggedIn = true
-const temperature = 41
+// const isUserLoggedIn = true
+// const temperature = 41
 
-if (temperature < 50){
-    console.log("less than 50"); 
+// if (temperature === 40){
+//     console.log("less than 50");
+// }
+// else {
+//     console.log("temperature is greater than 50");
+// }
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+const balance = 1000
+// if (balance > 500) console.log('test')
+
+// if (balance < 500) {
+//   console.log('less than')
+// } else if (balance < 750) {
+//   console.log("less than 750"); 
+// }else if (balance < 900) {
+//   console.log("less than 900");
+// }else{
+//   console.log("more than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+  console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+  console.log("user logged in");
 }
