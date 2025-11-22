2dd1801b2a6b0e319772e30de5edae19688c186f
Navneet Nihal <lakranihal0070@gmail.com>
2025-11-23 00:18:17 +0530

    promises and async await

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -82,16 +82,16 @@ async function consumePromiseFive() {
 }
 consumePromiseFive()
 
-// async function getAllusers() {
-//     try {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
-//         const data = await response.json()
-//         console.log(data);
-//     } catch (error) {
-//         console.log("E: ", error);
-//     }
-// };
-// getAllusers()
+async function getAllusers() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("E: ", error);
+    }
+};
+getAllusers()
 
 fetch('https://jsonplaceholder.typicode.com/users')
 .then((response) => {
